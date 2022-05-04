a5f5c63ff2e567ccb37489fe51a7de4758e6f782
Wellen Pereira <86011091+WellenPereiira@users.noreply.github.com>
2022-05-04 21:10:45 +0000

    criação do get por um  id especifico, do post, put e delete

--- index.js
+++ index.js
@@ -4,21 +4,36 @@ var { usuario } = require("./models")
 
 app.use(express.json());
 app.use(express.urlencoded({
-    extended: true
+  extended: true
 }));
 
-app.get("/", async function(req, res){
-    var resultado = await usuario.findAll(req.body);
-    res.json(resultado);
+app.get("/", async function(req, res) {
+  var resultado = await usuario.findAll(req.body);
+  res.json(resultado);
 });
 
-app.post("/", async function(req, res){
-    var resultado = await usuario.create(req.body);
-    res.json(resultado);
+app.get("/:id", async function(req, res) {
+  var resultado = await usuario.findOne({where:{ id:req.params.id }});
+  res.json(resultado);
 });
 
-app.listen(3000, function(){
-    console.log("O servidor está legalzão, está brugo demais e fumegando");
+app.post("/", async function(req, res) {
+  var resultado = await usuario.create(req.body);
+  res.json(resultado);
+});
+
+app.put("/:id", async function(req, res) {
+  var resultado = await usuario.update(req.body, {where:{ id:req.params.id }});
+  res.json(resultado);
+});
+
+app.delete("/:id", async function(req, res) {
+  var resultado = await usuario.destroy({where:{ id:req.params.id }});
+  res.json(resultado);
+});
+
+app.listen(3000, function() {
+  console.log("O servidor está legalzão, está brugo demais e fumegando");
 });
 
 //3 formas de pegar url
