d5e3a6b3aab92b56d47b37aa3f998949cbd14e89
Wellen Pereira <86011091+WellenPereiira@users.noreply.github.com>
2022-05-11 21:12:51 +0000

    criação do put, pot, delete e get para empresas

--- index.js
+++ index.js
@@ -1,42 +1,72 @@
 var express = require("express");
 var app = express();
 var { usuario } = require("./models")
+var { empresa } = require("./models")
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
 
-app.get("/", async function(req, res) {
+// ROTAS PARA USUÁRIO
+
+app.get("/usuarios", async function(req, res) {
   var resultado = await usuario.findAll(req.body);
   res.json(resultado);
 });
 
-app.get("/:id", async function(req, res) {
+app.get("/usuarios/:id", async function(req, res) {
   var resultado = await usuario.findOne({where:{ id:req.params.id }});
   res.json(resultado);
 });
 
-app.post("/", async function(req, res) {
+app.post("/usuarios", async function(req, res) {
   var resultado = await usuario.create(req.body);
   res.json(resultado);
 });
 
-app.put("/:id", async function(req, res) {
+app.put("/usuarios/:id", async function(req, res) {
   var resultado = await usuario.update(req.body, {where:{ id:req.params.id }});
   res.json(resultado);
 });
 
-app.delete("/:id", async function(req, res) {
+app.delete("/usuarios/:id", async function(req, res) {
   var resultado = await usuario.destroy({where:{ id:req.params.id }});
   res.json(resultado);
 });
 
-app.listen(3000, function() {
-  console.log("O servidor está legalzão, está brugo demais e fumegando");
+// ROTAS PARA EMPRESA
+
+app.get("/empresas", async function(req, res) {
+  var resultado = await empresa.findAll(req.body);
+  res.json(resultado);
 });
 
-//3 formas de pegar url
+app.get("/empresas/:id", async function(req, res) {
+  var resultado = await empresa.findOne({where:{ id:req.params.id }});
+  res.json(resultado);
+});
+
+app.post("/empresas", async function(req, res) {
+  var resultado = await empresa.create(req.body);
+  res.json(resultado);
+});
+
+app.put("/empresas/:id", async function(req, res) {
+  var resultado = await empresa.update(req.body, {where:{ id:req.params.id }});
+  res.json(resultado);
+});
+
+app.delete("/empresas/:id", async function(req, res) {
+  var resultado = await empresa.destroy({where:{ id:req.params.id }});
+  res.json(resultado);
+});
+
+app.listen(3000, function() {
+  console.log("O servidor está legalzão, está bruto demais e fumegando");
+});
+
+//3 formas de pegar a url
 //http://luizpicolo.com.br/inserir:?id=123 req.query.id
 //http://luizpicolo.com.br/inserir/123 req.params.id
 //http://luizpicolo.com.br/inserir     req.body.id
